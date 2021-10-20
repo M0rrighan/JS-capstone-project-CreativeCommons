4840c6ec816a8a31a402786b8fdb91b3b52f44d8
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-10-20 12:34:35 +0200

    Add src/cc-api.js: functions to fetch and process data from creative commons api

---
+++ src/cc-api.js
@@ -0,0 +1,35 @@
+/* eslint-disable import/prefer-default-export */
+const ccBaseUrl = 'https://api.creativecommons.engineering/v1/images';
+
+const fetchPage = async (pageNum) => {
+  const result = await fetch(`${ccBaseUrl}?categories=photograph&page=${pageNum}`);
+  return result.json();
+};
+
+const fetchItemDetails = async (id) => {
+  const result = await fetch(`${ccBaseUrl}/${id}`);
+  return result.json();
+};
+
+const getPageData = async (pageNum) => {
+  const dataFromApi = await fetchPage(pageNum);
+  return dataFromApi.results;
+};
+
+export const processPageResult = async (pageNum) => {
+  const returnedDataForPage = await getPageData(pageNum);
+  return [...returnedDataForPage]
+    .map((item) => ({ id: item.id, thumbnailUrl: item.thumbnail, title: item.title }));
+};
+
+export const getDetailsForItem = async (id) => {
+  const itemDetails = await fetchItemDetails(id);
+  return ({
+    title: itemDetails.title,
+    url: itemDetails.url,
+    creator: itemDetails.creator,
+    license: itemDetails.license,
+    source: itemDetails.source,
+    sourcePageUrl: itemDetails.foreign_landing_url,
+  });
+};
